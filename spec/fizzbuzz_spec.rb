743a2c3e27b047e1c9064f8fb8a5c2140bdff9ff
Sujeevan Rajagopalan <sujee_09@hotmail.com>
2020-11-16 13:47:40 +0000

    finished product

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+require './lib/fizzbuzz'
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when passed 15' do
+      expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    it 'returns "fizz" when passed 3' do
+      expect(fizzbuzz(3)).to eq 'fizz'
+    end
+    it 'returns "fizz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+    end
+end
